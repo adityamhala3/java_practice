f8ee8ac009eba6b3c60edf0692013cf6f1ee2a11
adityamhala3 <adityamhala3@gmail.com>
2026-07-04 17:09:51 +0530

    Time: 0 ms (100%), Space: 43.3 MB (79.14%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -12,10 +12,6 @@ class Solution {
 
 
 
-    // int[] target=new int[nums.length];
-    // for(int i=0;i<nums.length;i++){
-    //     target[index[i]]=nums[i];
-    // }
-    // return target;
+   
     }
 }
