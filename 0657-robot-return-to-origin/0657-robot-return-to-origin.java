0d6d7698c70a9b2ddb3a42269945c4e7c5c6f55f
adityamhala3 <adityamhala3@gmail.com>
2026-07-07 21:23:11 +0530

    Time: 7 ms (31.87%), Space: 45.2 MB (94.06%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,32 +1,62 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-       int x=0;
-       int y=0;
-       for(int i=0;i<moves.length();i++)
-       {
-            char c=moves.charAt(i);
-            switch(c)
-            {
-                case 'L':
-                    x++;
-                    break;
-                case 'R':
-                    x--;
-                    break;
-                case 'U':
-                    y++;
-                    break;
-                case 'D':
-                    y--;
-                    break;
+    //    int x=0;
+    //    int y=0;
+    //    for(int i=0;i<moves.length();i++)
+    //    {
+    //         char c=moves.charAt(i);
+    //         switch(c)
+    //         {
+    //             case 'L':
+    //                 x++;
+    //                 break;
+    //             case 'R':
+    //                 x--;
+    //                 break;
+    //             case 'U':
+    //                 y++;
+    //                 break;
+    //             case 'D':
+    //                 y--;
+    //                 break;
                 
-            }
+    //         }
       
-       }
-        if(x==0 && y==0){
-        return true;
+    //    }
+    //     if(x==0 && y==0){
+    //     return true;
 
+    //     }
+    //     return false;
+
+
+
+    int cR=0;
+    int cL=0;
+    int cU=0;
+    int cD=0;
+    for(int i=0;i<moves.length();i++){
+        char ch=moves.charAt(i);
+        if(ch=='L' ){
+            cL++;
+        
         }
-        return false;
+          else if(ch=='R' ){
+            cR++;
+        
+        }
+         else if(ch=='U' ){
+            cU++;
+        
+        }
+         else if(ch=='D' ){
+            cD++;
+        
+        }
+     }
+     if(cD==cU && cL==cR){
+        return true;
+     }
+     return false;
     }
 }
