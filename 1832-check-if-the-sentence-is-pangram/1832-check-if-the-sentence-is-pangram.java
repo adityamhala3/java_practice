506b2432eafaf0bb12fdd03a0dd121f122dbe64c
adityamhala3 <adityamhala3@gmail.com>
2026-07-18 00:31:02 +0530

    Time: 0 ms (100%), Space: 42.7 MB (93.18%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,11 +1,22 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean[] bo=new boolean[26];
-        for(char ch:sentence.toCharArray()){
-            bo[ch-'a']=true;//checking every letter and filling bo arr by true 
+        // boolean[] bo=new boolean[26];
+        // for(char ch:sentence.toCharArray()){
+        //     bo[ch-'a']=true;//checking every letter and filling bo arr by true 
+        // }
+        // for(boolean b:bo){
+        //     if(!b){//checking bo arr if there is any element which is false then that string is not a pangram
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+
+        if(sentence.length()<26){
+            return false;
         }
-        for(boolean b:bo){
-            if(!b){//checking bo arr if there is any element which is false then that string is not a pangram
+        for(char ch='a';ch<='z';ch++){
+            if(sentence.indexOf(ch)<0){
                 return false;
             }
         }
