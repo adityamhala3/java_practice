6383b563f029ef26a506b6d777b0990b1ff2c247
adityamhala3 <adityamhala3@gmail.com>
2026-07-25 19:05:47 +0530

    Time: 2 ms (19.36%), Space: 43.5 MB (61.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,36 +1,36 @@
 class Solution {
     public void sortColors(int[] nums) {
-      int low=0;
-      int mid=0;
-      int high=nums.length-1;
-      while(mid<=high){
-        if(nums[mid]==0){
-            int temp=nums[mid];
-            nums[mid]=nums[low];
-            nums[low]=temp;
-            low++;
-            mid++;
-        }
-        else if(nums[mid]==1){
-            mid++;
-        }
-        else{
-           int temp=nums[mid];
-           nums[mid]=nums[high];
-           nums[high]=temp;
-         
-           high--;
-        }
-      }
-    // for(int i=0;i<nums.length;i++){
-    //     for(int j=i+1;j<i;j++){
-    //         if(nums[j]<nums[i]){
-    //             int temp=nums[j];
-    //             nums[j]=nums[i];
-    //             nums[i]=nums[j];
-    //         }
+    //   int low=0;
+    //   int mid=0;
+    //   int high=nums.length-1;
+    //   while(mid<=high){
+    //     if(nums[mid]==0){
+    //         int temp=nums[mid];
+    //         nums[mid]=nums[low];
+    //         nums[low]=temp;
+    //         low++;
+    //         mid++;
     //     }
-    // }
+    //     else if(nums[mid]==1){
+    //         mid++;
+    //     }
+    //     else{
+    //        int temp=nums[mid];
+    //        nums[mid]=nums[high];
+    //        nums[high]=temp;
+         
+    //        high--;
+    //     }
+    //   }
+    for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[j]<nums[i]){
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+            }
+        }
+    }
     
     }
 }
