b2e6b1d259391bb31d5935d01d333477dd70966b
adityamhala3 <adityamhala3@gmail.com>
2026-08-03 19:43:58 +0530

    Time: 3 ms (14.82%), Space: 43.4 MB (61.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -23,11 +23,11 @@ class Solution {
     //     }
     //   }
     for(int i=0;i<nums.length;i++){
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[j]<nums[i]){
+        for(int j=1;j<nums.length-i;j++){
+            if(nums[j]<nums[j-1]){
                 int temp=nums[j];
-                nums[j]=nums[i];
-                nums[i]=temp;
+                nums[j]=nums[j-1];
+                nums[j-1]=temp;
             }
         }
     }
