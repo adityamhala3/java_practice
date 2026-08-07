b8c75d20e26636dc97d27618b39602bd1102884d
adityamhala3 <adityamhala3@gmail.com>
2026-08-07 19:03:21 +0530

    Time: 6 ms (83.09%), Space: 66.9 MB (92.39%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -10,16 +10,34 @@ class Solution {
         //         list.add(i);}
         // }
         // return list;
+//------------------------------------------------------------------------
+        //2nd soln
+        // HashSet<Integer> map=new HashSet<>();    
+        // for(int i:nums){          
+        //     map.add(i);          
+        // }
+        // for(int i=1;i<=nums.length;i++){
+        //     if(!map.contains(i)){
+        //         list.add(i);}
+        // }
+        // return list;
 
+  //---------------------------------------------------------------------
 
-        HashSet<Integer> map=new HashSet<>();    
-        for(int i:nums){          
-            map.add(i);          
-        }
-        for(int i=1;i<=nums.length;i++){
-            if(!map.contains(i)){
-                list.add(i);}
-        }
-        return list;
+  int i=0;
+  while(i<nums.length){
+    int c=nums[i]-1;
+    if(nums[i]!=nums[c]){
+        int temp=nums[i];
+        nums[i]=nums[c];
+        nums[c]=temp;
+    }else{i++;}
+  }
+  for(int j=0;j<nums.length;j++){
+    if(nums[j]!=j+1){
+        list.add(j+1);
+    }
+  }
+return list;
     }
 }
